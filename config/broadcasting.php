6d119fd60b2ad52d55c187d3d51be78259bc589e
Mostafa Nouh <mustafa.nouh.nouh@gmail.com>
2026-09-20 18:40:11 +0300

    update

---
+++ config/broadcasting.php
@@ -0,0 +1,50 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Default Broadcaster
+    |--------------------------------------------------------------------------
+    */
+
+    'default' => env('BROADCAST_CONNECTION', 'null'),
+
+    /*
+    |--------------------------------------------------------------------------
+    | Broadcast Connections
+    |--------------------------------------------------------------------------
+    */
+
+    'connections' => [
+
+        'reverb' => [
+            'driver' => 'reverb',
+
+            'key' => env('REVERB_APP_KEY'),
+
+            'secret' => env('REVERB_APP_SECRET'),
+
+            'app_id' => env('REVERB_APP_ID'),
+
+            'options' => [
+                'host' => env('REVERB_HOST', '127.0.0.1'),
+                'port' => env('REVERB_PORT', 8080),
+                'scheme' => env('REVERB_SCHEME', 'http'),
+                'useTLS' => env('REVERB_SCHEME', 'https') === 'https',
+            ],
+
+            'client_options' => [],
+        ],
+
+        'log' => [
+            'driver' => 'log',
+        ],
+
+        'null' => [
+            'driver' => 'null',
+        ],
+
+    ],
+
+];
